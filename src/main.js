bf86a95423a81670e52f09f82f18a60b976bc42d
florianLopitaux <florian.lopitaux@gmail.com>
2024-11-25 16:14:07 +0100

    Beginning to implement commands for each specs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,42 @@
 const cli = require("@caporal/core").default;
 
 cli
-    .argument("<name>", "Name to say Hello")
-    .action(({logger, args}) => {
-        logger.info("Hello %s !", args.name);
+    .version('SRU-software')
+    .version('0.01')
+
+    // SPEC 1
+    // command to show the rooms by session of a given course
+    .command('salles', 'display all sessions of the given course with associated rooms')
+    .argument('<course>', 'The name of the course')
+    .action(({args, logger}) => {
+        // TODO
     })
+
+    // SPEC 2
+    // command to show the maximum number of seats of a given room
+    .command('capacite', 'display the maximum number of seats of a given room')
+    .argument('<room_name>', 'The name of the room')
+    .action(({args, logger}) => {
+        // TODO
+    })
+
+    // SPEC 3
+    // command to show all free slots of a given room
+    .command('dispo_salle', 'display all free slots of a given room')
+    .argument('<room_name>', 'The name of the room')
+    .action(({args, logger}) => {
+        // TODO
+    })
+
+    // SPEC 4
+    // command to show all rooms unreserved of a given slot
+    .command('dispo_creneau', 'display all rooms unreserved of a given slot')
+    .argument('<day>', 'The day of the slot')
+    .option('-t, --time', 'Session start and end times following this format : HH:MM;HH:MM',
+        {validator: cli.STRING})
+    .action(({args, options, logger}) => {
+        // TODO
+    })
+
 
 cli.run();
