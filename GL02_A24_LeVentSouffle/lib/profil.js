5625b5d7c60ec1dea370a6669d539dfc922e9f90
Arbalight <Heliotness45@gmail.com>
2025-01-07 15:39:26 +0100

    Suppression du projet LeVentSouffle dans le projet GlowUTT

--- GL02_A24_LeVentSouffle/lib/profil.js
+++
@@ -1,9 +0,0 @@
-var profil = function(test /* ? */){
-	this.test = test;	
-	// A remplir : C'est le profil qui devra ensuite être transformé au format VCard
-   }
-
-   profil.prototype.equal=function(){
-    //
-	}
-   module.exports = profil;
